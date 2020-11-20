3139c49741c7d94762cf9cd777118907f6e644f8
Alison Lubar <aslubar@gmail.com>
2020-11-20 16:31:17 -0500

    created seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beyond Burger with Sriracha Mayo', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sirloin Burger with Caramelized Onions & Gruyere', false);
