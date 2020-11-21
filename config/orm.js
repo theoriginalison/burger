00dd729303a9ff78e1acf6533d70876ccb1cd26d
Alison Lubar <aslubar@gmail.com>
2020-11-20 19:33:51 -0500

    completed orm JS file

--- config/orm.js
+++ config/orm.js
@@ -2,4 +2,74 @@ var connection = require("../config/connection.js");
 
 //selectAll() insertOne() updateOne()
 
+
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+function objToSql(ob) {
+    var arr = [];
+
+    for (var key in ob) {
+        var value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+    return arr.toString();
+}
+
+var orm = {
+    selectAll: function (tableInput, cb) {
+        var queryString = `SELECT * FROM ${tableInput};`;
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    insertOne: function (tableInput, cols, vals, cb) {
+        var queryString = `INSERT INTO ${tableInput};`;
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    updateOne: function (tableInput, objColVals, condition, cb) {
+        var query = `UPDATE ${tableInput};`;
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+
+    }
+}
+
 module.exports = orm;
--- controllers/burgers-controller.js
+++ controllers/burgers-controller.js
@@ -1,6 +1,24 @@
 var express = require("express");
+const burger = require("../models/burger.js");
 
-var connection = require("../models/burger.js");
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+
+
+
+
 
 
 module.exports = router;
